4fe3f13636ec8f3fa0e3fb64c38152f52a8627bd
Nattaphat K.@home <nattaphat@gmail.com>
2013-03-28 22:41:01 +0700

    Add add user

--- src/Haii/IgisBundle/Controller/DefaultController.php
+++ src/Haii/IgisBundle/Controller/DefaultController.php
@@ -10,4 +10,30 @@ class DefaultController extends Controller
     {
         return $this->render('HaiiIgisBundle:Igis:index.html.twig');
     }
+
+	public function addUserFormAction()
+	{
+		return $this->render('HaiiIgisBundle:Igis:adduserform.html.twig');
+	}
+	
+	public function addUserAction()
+	{
+		return $this->render('HaiiIgisBundle:Igis:adduserform.html.twig');
+	}
+	
+	// public function load(ObjectManager $manager)
+	//     {
+	//         $user = new User();
+	//         $user->setUsername('admin');
+	//         $user->setSalt(md5(uniqid()));
+	// 
+	//         $encoder = $this->container
+	//             ->get('security.encoder_factory')
+	//             ->getEncoder($user)
+	//         ;
+	//         $user->setPassword($encoder->encodePassword('secret', $user->getSalt()));
+	// 
+	//         $manager->persist($user);
+	//         $manager->flush();
+	//     }
 }
